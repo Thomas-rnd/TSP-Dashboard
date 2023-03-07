cb1ac30922453ddc69f261bb5665a875892a994a
Thomas RENAUD <renaudthomas@orange.fr>
2023-03-07 22:16:10 +0100

    ajout choix random, ajout temps de calcul

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ app.layout = html.Div(
                             id="banner-logo",
                             children=[
                                 html.Img(src=app.get_asset_url(
-                                    "logo.png"))
+                                    "tsp-logo.jpg"))
                             ]
                         ),
                     ],
@@ -61,10 +61,10 @@ app.layout = html.Div(
             children=[
                 html.Div(
                     id="app-container",
-                    # className="row",
+                    className="row",
                     children=[
                         html.Div(
-                            # className="three columns",
+                            className="three columns",
                             id="left-column",
                             children=[
                                 drc.Card(
@@ -122,7 +122,117 @@ app.layout = html.Div(
                                 ),
                             ),
                         )
+                        ),
+                    ],
+                )
+            ],
+        ),
+        html.Div(
+            id="body",
+            className="container scalable",
+            children=[
+                html.Div(
+                    id="app-container",
+                    className="row",
+                    children=[
+                        html.Div(
+                            className="three columns",
+                            id="left-column",
+                            children=[
+                                drc.Card(
+                                    id="first-card",
+                                    children=[
+                                        drc.NamedDropdown(
+                                            name="Select Algorithme",
+                                            id="dropdown-select-algorithm-time",
+                                            options=[
+                                                {'label': '2-opt', 'value': 0},
+                                                {'label': 'Plus proche voisin',
+                                                 'value': 1},
+                                                {'label': 'Algorithme génétique',
+                                                    'value': 2}
+                                            ],
+                                            clearable=False,
+                                            searchable=False,
+                                            value=0,
+                                        ),
+                                    ]
+                                ),
+                            ],
+                        ),
+                        dcc.Loading(html.Div(
+                            id="div-graphs-bis",
+                            children=dcc.Graph(
+                                id="graph-tsp-bis",
+                                figure=dict(
+                                    layout=dict(
+                                        plot_bgcolor="#282b38", paper_bgcolor="#282b38"
+                                    )
+                                ),
+                            ),
                         )
+                        ),
+                    ],
+                )
+            ],
+        ),
+        html.Div(
+            id="body",
+            className="container scalable",
+            children=[
+                html.Div(
+                    id="app-container",
+                    className="row",
+                    children=[
+                        html.Div(
+                            className="three columns",
+                            id="left-column",
+                            children=[
+                                drc.Card(
+                                    id="first-card",
+                                    children=[
+                                        drc.NamedDropdown(
+                                            name="Select Algorithme",
+                                            id="dropdown-select-algorithm-random",
+                                            options=[
+                                                {'label': '2-opt', 'value': 0},
+                                                {'label': 'Plus proche voisin',
+                                                 'value': 1},
+                                                {'label': 'Algorithme génétique',
+                                                    'value': 2}
+                                            ],
+                                            clearable=False,
+                                            searchable=False,
+                                            value=0,
+                                        ),
+                                        drc.NamedSlider(
+                                            name="Sample Size",
+                                            id="slider-dataset-sample-size",
+                                            min=100,
+                                            max=500,
+                                            step=100,
+                                            marks={
+                                                str(i): str(i)
+                                                for i in [100, 200, 300, 400, 500]
+                                            },
+                                            value=300,
+                                        ),
+                                    ]
+                                ),
+                            ],
+                        ),
+                        dcc.Loading(html.Div(
+                            id="div-graphs-random",
+                            children=dcc.Graph(
+                                id="graph-tsp-random",
+                                figure=dict(
+                                    layout=dict(
+                                        plot_bgcolor="#282b38", paper_bgcolor="#282b38"
+                                    )
+                                ),
+                            ),
+                        )
+                        ),
                     ],
                 )
             ],
@@ -167,18 +277,13 @@ app.layout = html.Div(
 def update_data_test(num_dataset, algo):
     if algo == 0:
         df_unitaire, data = testTSP.test_unitaire_2_opt(int(num_dataset))
-        # df_global = testTSP.test_global_2_opt()
     elif algo == 1:
         df_unitaire, data = testTSP.test_unitaire_plus_proche_voisin(
             int(num_dataset))
-        # df_global = testTSP.test_global_plus_proche_voisin()
     else:
         df_unitaire, data = testTSP.test_unitaire_algo_genetique(
             int(num_dataset))
-        # df_global = testTSP.test_global_algo_genetique()
     solution_figure = affichage(df_unitaire, data)
-    # Figure de représentation du temps de calcul pour un algorithme
-    # figure_temps_calcul = representation_temps_calcul(df_global)
     return [
         html.Div(
             id="tsp-graph-container",
@@ -188,8 +293,66 @@ def update_data_test(num_dataset, algo):
                                    figure=solution_figure),
                 style={"display": "none"},
             ),
-        ),
-        """
+        ),]
+
+
+@ app.callback(
+    Output("div-graphs-random", "children"),
+    Input('slider-dataset-sample-size', 'value'),
+    Input('dropdown-select-algorithm-random', 'value')
+)
+def update_data_test(taille_dataset, algo):
+    # Initialisation du data frame avec TSPLIB
+    data = init_random_df(taille_dataset)
+
+    # Initialisation de la matrice des distances relatives
+    mat_distance = matrice_distance(data)
+
+    if algo == 0:
+        # On prend un chemin initial meilleur qu'un chemin aléatoire
+        # Attention cheminInitial est la liste des chemin exploré par l'algorithme
+        # plus_proche_voisin
+        cheminInitial, temps_calcul = src.plusProcheVoisin.plus_proche_voisin(
+            data, mat_distance)
+
+        # Lancement de l'algorithme 2-opt
+        df_res = src.algo2Opt.main(
+            mat_distance, cheminInitial[-1])
+    elif algo == 1:
+        # Lancement de l'algorithme plus proche voisin
+        df_res = src.plusProcheVoisin.main(data, mat_distance)
+    else:
+        df_res = src.geneticAlgorithm.main(data, mat_distance)
+
+    # La solution trouvée par l'algo choisi
+    solution_figure = affichage(df_res, data)
+
+    return [
+        html.Div(
+            id="tsp-graph-container",
+            children=dcc.Loading(
+                className="graph-wrapper",
+                children=dcc.Graph(id="graph-tsp",
+                                   figure=solution_figure),
+                style={"display": "none"},
+            ),
+        ),]
+
+
+@ app.callback(
+    Output("div-graphs-bis", "children"),
+    Input('dropdown-select-algorithm-time', 'value')
+)
+def update_data_test(algo):
+    if algo == 0:
+        df_global = testTSP.test_global_2_opt()
+    elif algo == 1:
+        df_global = testTSP.test_global_plus_proche_voisin()
+    else:
+        df_global = testTSP.test_global_algo_genetique()
+    # Figure de représentation du temps de calcul pour un algorithme
+    figure_temps_calcul = representation_temps_calcul(df_global)
+    return [
         html.Div(
             id="graphs-container",
             children=[
@@ -199,7 +362,7 @@ def update_data_test(num_dataset, algo):
                         id="graph-line-roc-curve", figure=figure_temps_calcul),
                 ),
             ],
-        ),"""
+        ),
     ]
 
 
