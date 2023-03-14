ea6099ff047914ef8bade990598bbac42e1f9861
Thomas RENAUD <renaudthomas@orange.fr>
2023-03-14 18:05:35 +0100

    algo génétique sans temps de calcul

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-from dash import Dash, Input, Output, dcc, html, ctx
+import plotly.express as px
+from dash import Dash, Input, Output, ctx, dcc, html
 from dash.dependencies import Input, Output, State
 
-import src.algo2Opt
-import src.geneticAlgorithm
-import src.plusProcheVoisin
-import src.testTSPLIB as testTSP
+import src.algo_2_opt
+import src.algo_genetique
+import src.algo_proche_voisin
+import src.test_TSPLIB
 import utils.dash_reusable_components as drc
 from src.distance import matrice_distance
 from src.graph import affichage, representation_temps_calcul
-from src.randomData import init_random_df
-from src.testData import data_TSPLIB, tour_optimal
+from src.random_data import init_random_df
 
 app = Dash(
     __name__,
@@ -69,13 +69,14 @@ app.layout = html.Div(
                                     id="first-card",
                                     children=[
                                         drc.NamedDropdown(
-                                            name="Select Algorithme",
+                                            name="Select Algorithm",
                                             id="dropdown-select-algorithm",
                                             options=[
-                                                {'label': '2-opt', 'value': 0},
-                                                {'label': 'Plus proche voisin',
+                                                {'label': '2-opt inversion',
+                                                    'value': 0},
+                                                {'label': 'Nearest neighbor search',
                                                  'value': 1},
-                                                {'label': 'Algorithme génétique',
+                                                {'label': 'Genetic algorithm',
                                                     'value': 2}
                                             ],
                                             clearable=False,
@@ -83,7 +84,7 @@ app.layout = html.Div(
                                             value=0,
                                         ),
                                         drc.NamedSlider(
-                                            name="Sample Size",
+                                            name="Number of cities to explore",
                                             id="slider-dataset-sample-size",
                                             min=10,
                                             max=500,
@@ -105,7 +106,7 @@ app.layout = html.Div(
                                     children=dcc.Loading(
                                         className="graph-wrapper",
                                         children=dcc.Graph(
-                                            id="graph-sklearn-svm"),
+                                            id="graph-tsp"),
                                         style={"display": "none"},
                                     ),
                                 ),
@@ -115,7 +116,7 @@ app.layout = html.Div(
                                         dcc.Loading(
                                             className="graph-wrapper",
                                             children=dcc.Graph(
-                                                id="graph-line-roc-curve"),
+                                                id="graph-calculus-time"),
                                         ),
                                     ],
                                 ),
@@ -130,7 +131,7 @@ app.layout = html.Div(
 
 
 @ app.callback(
-    Output("graph-sklearn-svm", "figure"),
+    Output("graph-tsp", "figure"),
     Input('slider-dataset-sample-size', 'value'),
     Input('dropdown-select-algorithm', 'value')
 )
@@ -145,17 +146,17 @@ def update_random_graph(taille_dataset, algo):
         # On prend un chemin initial meilleur qu'un chemin aléatoire
         # Attention cheminInitial est la liste des chemin exploré par l'algorithme
         # plus_proche_voisin
-        cheminInitial, temps_calcul = src.plusProcheVoisin.plus_proche_voisin(
+        chemin_initial, temps_calcul = src.algo_proche_voisin.plus_proche_voisin(
             data, mat_distance)
 
         # Lancement de l'algorithme 2-opt
-        df_res = src.algo2Opt.main(
-            mat_distance, cheminInitial[-1])
+        df_res = src.algo_2_opt.main(
+            mat_distance, chemin_initial[-1])
     elif algo == 1:
         # Lancement de l'algorithme plus proche voisin
-        df_res = src.plusProcheVoisin.main(data, mat_distance)
+        df_res = src.algo_proche_voisin.main(data, mat_distance)
     else:
-        df_res = src.geneticAlgorithm.main(data, mat_distance)
+        df_res = src.algo_genetique.main(data, mat_distance)
 
     # La solution trouvée par l'algo choisi
     solution_figure = affichage(df_res, data)
@@ -164,18 +165,18 @@ def update_random_graph(taille_dataset, algo):
 
 
 @ app.callback(
-    Output("graph-line-roc-curve", "figure"),
+    Output("graph-calculus-time", "figure"),
     Input('dropdown-select-algorithm', 'value')
 )
-def update_random_graph(algo):
+def update_temps_graph(algo):
     # On récupère l'id de l'input ayant été modifié
 
     if algo == 0:
-        df_global = testTSP.test_global_2_opt()
+        df_global = src.test_TSPLIB.test_global_2_opt()
     elif algo == 1:
-        df_global = testTSP.test_global_plus_proche_voisin()
+        df_global = src.test_TSPLIB.test_global_plus_proche_voisin()
     else:
-        df_global = testTSP.test_global_algo_genetique()
+        return px.line()
     # Figure de représentation du temps de calcul pour un algorithme
     figure_temps_calcul = representation_temps_calcul(df_global)
 
